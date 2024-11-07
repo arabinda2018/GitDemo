01a9ef046773b4fb2c3cbc15d8fc433a9cc3044b
Arabinda Mohanty <arabindamohanty41@gmail.com>
2024-11-07 18:06:42 +0530

    this is commit from asian time zone

--- src/collectionconcepts/A.java
+++ src/collectionconcepts/A.java
@@ -5,23 +5,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class A {
-public static void main(String[] args) {
-	ArrayList<String>a = new ArrayList<String>();
-	a.add("Jan");
-	a.add("feb");
-	a.add("Jan");
-	a.add("apr");
-	a.add("may");
-	Set<String> s = new HashSet<String>();
-	for(String m:a) {
-		if(s.add(m)==false) {
-			//System.out.println(m+"is duplicate");
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("Jan");
+		a.add("feb");
+		a.add("Jan");
+		a.add("apr");
+		a.add("may");
+		a.add("june");
+		Set<String> s = new HashSet<String>();
+		for (String m : a) {
+			if (s.add(m) == false) {
+				// System.out.println(m+"is duplicate");
+			} else {
+				System.out.println(m + "all are unique");
+			}
 		}
-		else {
-			System.out.println(m+"all are unique");
-		}
+
 	}
-	
-	
-}
 }
